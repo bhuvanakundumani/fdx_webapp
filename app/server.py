6643306ea1236aab1f3cebb8d65b116e2e1d34f2
bhuvanakundumani <bhuvana.kundumani@gmail.com>
2019-04-10 13:31:32 +0530

    Changed the version of Fastai to fix ImageItemList error

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1wYezID76VTfxgvIUBmad8qnNBBAYyEMm'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-0Mb-pnM3MUPO2Eex4YqzUa2c_62XOXP'
 export_file_name = 'export.pkl'
 
 classes = ['11G', '11H', '12G', '17D', '5E']
